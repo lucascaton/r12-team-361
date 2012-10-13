7d784cc6c68e8579ece4315a23b16c0114916a1f
Lucas Caton <lucascaton@gmail.com>
2012-10-13 15:59:50 -0300

    Improving specs configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,14 @@
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join('spec/support/**/*.rb')].each {|f| require f}
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  config.order = 'random'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
--- spec/unit_test.rb
+++
@@ -1,2 +0,0 @@
-rails_root = File.expand_path('../../', __FILE__)
-$LOAD_PATH.unshift(rails_root) unless $LOAD_PATH.include?(rails_root)
