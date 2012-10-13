e9901c6fe63eb237bf6cd140993e5b6f8c0f0ecc
Lucas Caton <lucascaton@gmail.com>
2012-10-12 23:03:23 -0300

    Initial Rails directory structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BabyCasts::Application.config.secret_token = 'e54503e8d6e7cb1bf947870cbecaa7a7fd6a8da870b5e38f516236988b5e8a751ddb911d34ad6e88c7e12de682c517d6e125876de1c6fa5f55b4e723a0c59769'
