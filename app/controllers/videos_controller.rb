330f7c5fc08764ce6f9f923117c5f1ad7c121f8e
Tomas D'Stefano <tomas_stefano@successoft.com>
2012-10-14 16:05:51 -0300

    Remove not used actions from videos controller. Will be add when it needs. :)

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -16,42 +16,6 @@ class VideosController < ApplicationController
     @video = Video.find params[:id]
   end
 
-  def new
-    @video = Video.new
-  end
-
-  def create
-    @video = Video.new params[:video]
-
-    if @video.save
-      redirect_to @video
-    else
-      flash[:erorr] = @video.errors.full_messages.first
-      render :new
-    end
-  end
-
-  def edit
-    @video = Video.find params[:id]
-  end
-
-  def update
-    @video = Video.find params[:id]
-
-    if @video.update_attributes params[:video]
-      redirect_to @video
-    else
-      flash[:erorr] = @video.errors.full_messages.first
-      render :edit
-    end
-  end
-
-  def destroy
-    @video = Video.find params[:id]
-    @video.destroy
-    redirect_to videos_path
-  end
-
   protected
 
   def assign_user
