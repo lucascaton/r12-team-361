bb7b36a0ffb4d7f0119a076f219725d9fd4fb0d7
Tomas D'Stefano <tomas_stefano@successoft.com>
2012-10-14 16:25:21 -0300

    Adding methods to the video presenter on the sponsored rule

--- app/presenters/video_presenter.rb
+++ app/presenters/video_presenter.rb
@@ -1,2 +1,13 @@
 class VideoPresenter < Presenter
+  def sponsored_mark
+    content_tag(:span, "sponsored") if sponsored?
+  end
+
+  def presentation
+    if sponsored?
+      "video-content span9 sponsored"
+    else
+      "video-content span9"
+    end
+  end
 end
--- spec/presenters/video_presenter_spec.rb
+++ spec/presenters/video_presenter_spec.rb
@@ -1,4 +1,39 @@
 require 'spec_helper'
 
 describe VideoPresenter do
+  describe "#sponsored_mark" do
+    context "when is a sponsor video" do
+      let(:video) { VideoPresenter.new(stub(:sponsored? => true)) }
+
+      it "should return the sponsored mark" do
+        expect(video.sponsored_mark).to eq "<span>sponsored</span>"
+      end
+    end
+
+    context "when isn't a sponsor video" do
+      let(:video) { VideoPresenter.new(stub(:sponsored? => false)) }
+
+      it "should return nothing" do
+        expect(video.sponsored_mark).to be_nil
+      end
+    end
+  end
+
+  describe "#presentation" do
+    context "when is a sponsor video" do
+      let(:video) { VideoPresenter.new(stub(:sponsored? => true)) }
+
+      it "should return the sponsored mark" do
+        expect(video.presentation).to eq "video-content span9 sponsored"
+      end
+    end
+
+    context "when isn't a sponsor video" do
+      let(:video) { VideoPresenter.new(stub(:sponsored? => false)) }
+
+      it "should return the video content normally without sponsored" do
+        expect(video.presentation).to eq "video-content span9"
+      end
+    end
+  end
 end
