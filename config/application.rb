94103a0d814021bc989e23d8ff530a704ab17acf
Joao Moura <joaomdmoura@gmail.com>
2012-10-15 11:33:36 -0300

    changing asssets version

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,6 @@ module BabyCasts
     config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '2.0'
   end
 end
