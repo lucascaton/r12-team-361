f9fc5f3335b957cc15b72e00ef822019e554eccb
Joao Moura <joaomdmoura@gmail.com>
2012-10-15 11:35:39 -0300

    rolling back assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module BabyCasts
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '2.0'
