cda2da7cbb2d4c74da114577f28a1739527c8077
Joao Moura <joaomdmoura@gmail.com>
2012-10-15 11:58:35 -0300

    fixing environment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ BabyCasts::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
